ac599b1c224b692cdb1d2a7401088b7bf9047354
EvertonAraujo88 <evertonaraujosenai@gmail.com>
2023-10-10 19:21:20 -0300

    java poo

---
+++ Java/POO/src/PessoaJuridica.java
@@ -0,0 +1,27 @@
+public class PessoaJuridica extends Pessoa{
+
+    public String CNPJ;
+    public String RazaoSocial;
+
+    @Override
+    public float CalcularImposto(float rendimento) {
+    //      até 3000 - 3%
+    //      de 3000 até 6000 - 5%
+    //      de 6000 até 10000 - 7%
+    //      acima de 10000 - 9%
+
+        if (rendimento <= 3000) {
+            return rendimento * .03f;
+
+        } else if (rendimento <= 6000) {
+            return rendimento * .05f;
+
+        } else if (rendimento <= 10000) {
+            return rendimento * .07f;
+        } else {
+            return rendimento * .09f;
+        }
+
+    }
+}
+
