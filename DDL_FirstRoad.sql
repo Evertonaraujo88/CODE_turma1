46b8a82321bbe2f3b6d7a209543c44e219b038a8
EvertonAraujo88 <evertonaraujosenai@gmail.com>
2024-02-20 18:25:33 -0300

    kotlin

--- DDL_FirstRoad.sql
+++ DDL_FirstRoad.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE FirstRoad;
 
+drop database firstroad;
 USE FirstRoad;
 
 CREATE TABLE tb_TiposUsuario(
 	Id_tipoUsuario BINARY(16) NOT NULL,
 	tituloTipoUsuario VARCHAR(50) NOT NULL ,
+    
     PRIMARY KEY(Id_tipoUsuario)
 );
 
@@ -17,12 +19,14 @@ CREATE TABLE tb_Unidades(
     complemento VARCHAR(100),
     bairro VARCHAR (200) NOT NULL,
     cep VARCHAR(9) NOT NULL,
+    
     PRIMARY KEY(Id_unidade)
 );
 
 CREATE TABLE tb_Cargo(
 	Id_cargo BINARY(16) NOT NULL,
 	nomeCargo VARCHAR(50) NOT NULL,
+    
     PRIMARY KEY(Id_cargo)
 );
 
@@ -32,45 +36,52 @@ CREATE TABLE tb_Conteudo(
     descricaoCOnteudo VARCHAR(255) NOT NULL,
     link VARCHAR(255) NOT NULL,
     tempoConteudo time NOT NULL,
+    
     PRIMARY KEY(Id_conteudo)
 );
 
+
 CREATE TABLE tb_Usuarios(
-	Id_usuario BINARY(60) NOT NULL,
+	Id_usuario BINARY(16) NOT NULL,
     nomeUsuario VARCHAR(255) NOT NULL,
     email VARCHAR (100) NOT NULL UNIQUE,
     senha VARCHAR (60) NOT NULL,
     nif INT NOT NULL UNIQUE,
     dataNascimento DATE NOT NULL,
-    Id_tipoUsuario BINARY(60)NOT NULL,
-	Id_unidade BINARY(60)NOT NULL,
-	Id_cargo BINARY(60) NOT NULL,
-        PRIMARY KEY(Id_usuario),
+    
+    Id_tipoUsuario BINARY(16)NOT NULL,
+	Id_unidade BINARY(16)NOT NULL,
+	Id_cargo BINARY(16) NOT NULL,
+    
+	PRIMARY KEY(Id_usuario),
     FOREIGN KEY(Id_tipoUsuario) REFERENCES tb_TiposUsuario(Id_tipoUsuario),
-    FOREIGN KEY(Id_unidade) REFERENCES tb_Usuarios(Id_usuario),
+    FOREIGN KEY(Id_unidade) REFERENCES tb_Unidades(Id_unidade),
     FOREIGN KEY(Id_cargo) REFERENCES tb_Cargo(Id_cargo)
 );
 
 CREATE TABLE tb_UnidadesOrganizacionais(
 	Id_uo BINARY(16) NOT NULL,
-    nomeDepartament VARCHAR (200)NOT NULL,
+    nomeDepartamento VARCHAR (200)NOT NULL,
+    
     Id_usuario BINARY(16) NOT NULL,
 	Id_unidade BINARY(16) NOT NULL,
+    
 	PRIMARY KEY(Id_uo),
     FOREIGN KEY(Id_usuario) REFERENCES tb_usuarios(Id_usuario),
     FOREIGN KEY(Id_unidade) REFERENCES tb_unidades(Id_unidade)
 );
-
+ 
 CREATE TABLE tb_Trilha(
 	Id_trilha BINARY(16) NOT NULL,
-    nomeDepartament VARCHAR (200) NOT NULL,
     tituloTrilha VARCHAR (100) NOT NULL,
     descricaoTrilha VARCHAR (255) NOT NULL,
     tempoTrilha TIME NOT NULL,
     situacao VARCHAR (150) NOT NULL,
+    
     Id_cargo BINARY(16) NOT NULL,
 	Id_uo BINARY(16) NOT NULL,
     Id_conteudo BINARY(16) NOT NULL,
+    
 	PRIMARY KEY(Id_trilha),
     FOREIGN KEY(Id_cargo) REFERENCES tb_cargo(Id_cargo),
     FOREIGN KEY(Id_uo) REFERENCES tb_UnidadesOrganizacionais(Id_uo),
--- DML_FirstRoad.sql
+++ DML_FirstRoad.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE FirstRoad;
+
+USE FirstRoad;
 
 
 INSERT INTO tb_tiposusuario VALUES(
@@ -51,22 +52,43 @@ SELECT BIN_TO_UUID(Id_cargo), nomeCargo FROM tb_cargo;
 
  INSERT INTO tb_conteudo VALUES(
 	UUID_TO_BIN(UUID()),
-    "Padrão de BEM",
-    "Esse conteúdo trata-se de um treinamento para boas pricaticas e processo de nomenclatura de código",
-    "https://www.alura.com.br/",
-    "00:15:00"
+    "SpringBoot",
+    "Aprendendo SpringBoot do zero ao avançado!!",
+    "https://www.udemy.com.br/",
+    "00:17:00"
     );
 SELECT BIN_TO_UUID(Id_conteudo), tituloConteudo, descricaoConteudo, link, tempoConteudo FROM tb_conteudo;
 
  INSERT INTO tb_usuarios VALUES(
 	UUID_TO_BIN(UUID()),
-    "Everton Araujo",
-    "everton@vw.com",
+    "Lucas Lessa",
+    "lessa@vw.com",
     "senai",
-    "123456",
-    "1988-04-04",
-    "6d488372-7910-11ee-8704-6018955666f8",
-    "bdadc69c-7915-11ee-8704-6018955666f8",
-    "3f403f12-7916-11ee-8704-6018955666f8"
-    );
+    "789456",
+    "1994-05-19",
+    UUID_TO_BIN("6586a940-8017-11ee-93e3-6018955666f8"),/*tipoUsuario*/
+    UUID_TO_BIN("71a1f4ce-8017-11ee-93e3-6018955666f8"),/*Unidade*/
+    UUID_TO_BIN("88eca334-8017-11ee-93e3-6018955666f8")/*cargo*/    
+);
 
+select BIN_TO_UUID(Id_usuario), nomeUsuario from tb_Usuarios;
+
+INSERT INTO tb_UnidadesOrganizacionais VALUES(
+	UUID_TO_BIN(UUID()),
+    "TI",
+    UUID_TO_BIN("d8873aa7-8017-11ee-93e3-6018955666f8"),
+    UUID_TO_BIN("71a1f4ce-8017-11ee-93e3-6018955666f8")
+);    
+select BIN_TO_UUID(Id_uo), nomeDepartament from tb_UnidadesOrganizacionais;
+
+INSERT INTO tb_Trilha VALUES(
+	UUID_TO_BIN(UUID()),
+    "Trilha Dev iniciante",
+    "Esta trilha contem passos iniciais para um dev iniciante",
+    "",
+    "",
+    UUID_TO_BIN("88eca334-8017-11ee-93e3-6018955666f8"),/*Id_cargo*/
+    UUID_TO_BIN("2aff301d-801a-11ee-93e3-6018955666f8"),/*Id_uo*/
+    UUID_TO_BIN("a9411e4a-8018-11ee-93e3-6018955666f8")/*Id_conteudo*/
+);
+SELECT BIN_TO_UUID(Id_trilha), tituloTrilha, descricaoTrilha, tempoTrilha, situacao, (Id_cargo), (Id_uo), (Id_conteudo) from tb_Trilha;
